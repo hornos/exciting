af20bbecccb25b8494392bf787e7dcea0b834340
Tom Hornos <tom.hornos@gmail.com>
2010-11-19 12:53:37 +0100

    infile rules args objdep.py

--- exc/build/objdep.py
+++ exc/build/objdep.py
@@ -1,26 +1,64 @@
 #!/usr/bin/python
+
 #
-# TODO: args
+# The simple minded object generator for Fortran
 #
 
+### includes
 import os
 import sys
 import re
 import string
 import fileinput
+import ConfigParser
+from optparse import OptionParser
 
-debug = False
 
-src_pat   = "src_.*"
-root_path = "."
-fex_arr  = [ "\.F(90)?$", "\.f(90)?$" ]
+### defaults
+comp_cmd="$(FC) $(FFLAGS) $(INCLUDES)"
 
+
+### cmd args
+opt_parser  = OptionParser()
+
+opt_parser.add_option( "-o", "--output",
+                       action = "store", type = "string",
+                       dest = "output", default = 'Makefile.objdep',
+                       help = "Output Makefile" )
+
+opt_parser.add_option( "-f", "--fex",
+                       action = "store", type = "string",
+                       dest = "fex_arr", default = '\.F(90)?$:\.f(90)?$',
+                       help = "Fortran extension patterns to scan" )
+
+opt_parser.add_option( "-s", "--src",
+                       action = "store", type = "string",
+                       dest = "src_pat", default = 'src_.*',
+                       help = "Source directory pattern" )
+
+opt_parser.add_option( "-r", "--root",
+                       action = "store", type = "string",
+                       dest = "root_path", default = '.',
+                       help = "Root directory" )
+
+opt_parser.add_option( "-d", "--debug",
+                        action = "store_true",
+                        dest = "debug", default = False,
+                        help = "Debug verbose" )
+### process args
+(options, args) = opt_parser.parse_args()
+debug     = options.debug
+src_pat   = options.src_pat
+root_path = options.root_path
+fex_arr   = options.fex_arr.split(":")
+output    = options.output
+
+### lookup tables
 obj_db = {}
 mod_db = {}
 
 
-comp_cmd="$(FC) $(FFLAGS) $(INCLUDES)"
-
+### functions
 def brkstr( a, m = 5 ):
   r = ""
   c = 1
@@ -37,9 +75,12 @@ def brkstr( a, m = 5 ):
   return r
 # end def
 
+
+### BEGIN MAIN
 try:
-  MF = open( "Makefile.objdep", "w" )
+  MF = open( output, "w" )
 except:
+  print "Output error: " + output
   sys.exit(1)
 
 
@@ -74,10 +115,30 @@ for root, dirs, files in os.walk( root_path ):
       # search source
       tmp = []
       modules = []
+      rules = {}
       uses = []
       includes = []
       for line in fileinput.input( fp ):
         line = line.strip()
+
+        # infile rules
+        pat = re.compile( "^!RULE +FC:.*", re.IGNORECASE )
+        if re.match( pat, line ):
+          tmp = line.split( ":" )
+          rules["FC"] = tmp[1].strip()
+        # end if
+
+        pat = re.compile( "^!RULE +FFLAGS:.*", re.IGNORECASE )
+        if re.match( pat, line ):
+          tmp = line.split( ":" )
+          rules["FFLAGS"] = tmp[1].strip()
+        # end if
+
+        pat = re.compile( "^!RULE +INCLUDES:.*", re.IGNORECASE )
+        if re.match( pat, line ):
+          tmp = line.split( ":" )
+          rules["INCLUDES"] = tmp[1].strip()
+        # end if
 
         # module
         pat = re.compile( "^MODULE +.*$", re.IGNORECASE )
@@ -106,6 +167,11 @@ for root, dirs, files in os.walk( root_path ):
             includes.append( root+"/"+tmp[1] )
         # end if
       # end for line
+
+      obj_db[fon] = { "path" : fpl, "opath" : fp, 
+                      "module" : modules, "use" : uses, 
+                      "include" : includes }
+
       if debug:
         if modules:
           print "  MODULE :", modules
@@ -113,9 +179,17 @@ for root, dirs, files in os.walk( root_path ):
           print "  USE :", uses
         if includes:
           print "  INCLUDE :", includes
+        if "FC" in rules:
+          obj_db[fon].update( { "rule_fc" : rules["FC"] } )
+          print "  RULE FC:", rules["FC"]
+        if "FFLAGS" in rules:
+          obj_db[fon].update( { "rule_fflags" : rules["FFLAGS"] } )
+          print "  RULE FFLAGS:", rules["FFLAGS"]
+        if "INCLUDES" in rules:
+          obj_db[fon].update( { "rule_includes" : rules["INCLUDES"] } )
+          print "  RULE INCLUDES:", rules["INCLUDES"]
       # end if
 
-      obj_db[fon] = { "path" : fpl, "opath" : fp, "module" : modules, "use" : uses, "include" : includes }
 
     # end for fex
 
@@ -150,7 +224,17 @@ for obj in obj_db:
 
   dep_str = brkstr( dep )
   MF.write( obj + " : " + dep_str + "\n" )
-  MF.write( "\t"+comp_cmd+" -c "+obj_db[obj]["opath"]+" -o "+obj+"\n" )
+
+  # prepare compilation command
+  ccmd = comp_cmd
+  if "rule_fc" in obj_db[obj]:
+    ccmd = ccmd.replace( "$(FC)", obj_db[obj]["rule_fc"] )
+  if "rule_fflags" in obj_db[obj]:
+    ccmd = ccmd.replace( "$(FFLAGS)", obj_db[obj]["rule_fflags"] )
+  if "rule_includes" in obj_db[obj]:
+    ccmd = ccmd.replace( "$(INCLUDES)", obj_db[obj]["rule_includes"] )
+
+  MF.write( "\t"+ccmd+" -c "+obj_db[obj]["opath"]+" -o "+obj+"\n" )
 # end for
 
 MF.write( "\n\n### MiSC ###\n" )
@@ -158,3 +242,4 @@ MF.write( "OBJS = " + brkstr( objs ) + "\n\n" )
 MF.write( "SRCS = " + brkstr( srcs ) + "\n\n" )
 
 MF.close()
+### END MAIN
